e2be3f3c93a54304b9a3b2d59329b4b99b9db71e
moreaditi-tech <morerajaditi@gmail.com>
2026-09-06 13:05:55 +0530

    Move India map down to avoid navbar cutoff

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -513,7 +513,7 @@ export default function HomePage() {
             </div>
 
             {/* Right Map Graphic */}
-            <div className="lg:col-span-7 relative flex justify-center lg:justify-end reveal-up">
+            <div className="lg:col-span-7 relative flex justify-center lg:justify-end reveal-up mt-8 lg:mt-16">
               <div className="relative w-full max-w-[650px] aspect-[4/3] sm:aspect-square drop-shadow-[0_0_35px_rgba(201,164,92,0.12)]">
                 <Image 
                   src="/images/areas/india-map.png"
